e9ac5fea7160ebe0be4602e9386ebb0aa0fdfdfa
sonalishinde30 <95482639+sonalishinde30@users.noreply.github.com>
2022-01-17 19:12:37 +0530

    Create main.js

---
+++ Task_no_1/js/main.js
@@ -0,0 +1,9 @@
+var navlinks = document.getElementById("navLinks");
+
+    function showMenu() {
+      navlinks.style.right = "0";
+    }
+
+    function hideMenu() {
+      navlinks.style.right = "-200px";
+    }
